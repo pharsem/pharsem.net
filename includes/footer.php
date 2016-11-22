426eddee797379d6cfc701dafe3ce52cc944bfd3
Petter Harsem <petter.harsem@gmail.com>
2016-11-22 14:19:41 +0100

    Footer links open in new tab

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,9 @@
 <footer>
     <ul>
-        <li id="email"><a href="mailto:hello@pharsem.net"></a></li>
-        <li id="twitter"><a href="https://twitter.com/pharsem"></a></li>
-        <li id="facebook"><a href="https://www.facebook.com/harsem"></a></li>
-        <li id="linkedin"><a href="https://www.linkedin.com/in/pharsem"></a></li>
-        <li id="github"><a href="https://github.com/pharsem"></a></li>
+        <li id="email"><a href="mailto:hello@pharsem.net" target="_blank"></a></li>
+        <li id="twitter"><a href="https://twitter.com/pharsem" target="_blank"></a></li>
+        <li id="facebook"><a href="https://www.facebook.com/harsem" target="_blank"></a></li>
+        <li id="linkedin"><a href="https://www.linkedin.com/in/pharsem" target="_blank"></a></li>
+        <li id="github"><a href="https://github.com/pharsem" target="_blank"></a></li>
     </ul>
 </footer>
